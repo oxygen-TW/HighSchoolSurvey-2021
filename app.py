6acd3a67ea715ae914ea8a82d89b9c6ed0ee5333
oxygen-TW <howard2805@gmail.com>
2021-03-14 13:16:35 +0800

    Add schema

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, Response, send_from_directory
-from flask import render_template
 from flask import jsonify
 from pymysql import NULL
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-import time
 
-import requests
+from flask_pydantic import validate
+
+import time
 
 from database import Database
 from config import schoolCodeDict, MainConfig
 from auth import verify, generateJWTtoken
+from schema import frontData
 
 app = Flask(__name__)
 
@@ -38,54 +39,63 @@ def authFunction():
     return jsonify(verifyRes), 200
 
 @app.route("/submit", methods=['POST'])
+@validate()
 @jwt_required()
-def submit():
+def submit(body:frontData):
     current_user = get_jwt_identity()
     print(current_user)
     # 建立資料庫控制器
     d = Database("Main")
 
     # 檢查學校代碼
-    schoolCode = request.json["schoolCode"]
+    schoolCode = request.json["school"]
     if(schoolCode not in schoolCodeDict):
         schoolCode = NULL
 
         err = {
             "msg":"學校代碼錯誤",
-            "request": request.json["schoolCode"]
+            "request": request.json["school"]
         }
         return jsonify(str(err)), 400
 
     else:
-        schoolCode = schoolCodeDict[request.json["schoolCode"]]
+        schoolCode = schoolCodeDict[request.json["school"]]
         print(request.json["stuId"])
 
     # 檢查學號是否重複，並且防止SQL注入
     result = d.checkStuId(request.json["stuId"], schoolCode)
-    print(result)
+
     if(result and (request.json["stuId"] != MainConfig["bypassCode"])):
         return jsonify(str({
             "msg": "re-submit data"
         })), 400
 
     # 建立資料
-    data = {
-        "stuId": request.json["stuId"],
-        "school": schoolCode,
-        "sex": request.json["sex"],
-        "grade": request.json["grade"],
-        "department": request.json["dept"],
-        "Q1": request.json["Q1"],
-        "Q2": request.json["Q2"],
-        "Q3": request.json["Q3"],
-        "Q4": request.json["Q4"],
-        "Q5": request.json["Q5"],
-        "Q6": request.json["Q6"],
-        "Q7": request.json["Q7"],
-    }
+    Q2 = ""
+    Q6 = ""
+    if("Q2" in request.json):
+       Q2 = request.json["Q2"]
 
+    if("Q6" in request.json):
+        Q6 = request.json["Q6"]
+        
+    data = frontData(
+        stuId = request.json["stuId"],
+        school = schoolCode,
+        sex = request.json["sex"],
+        grade = request.json["grade"],
+        department = request.json["department"],
+        Q1 = request.json["Q1"],
+        Q2 = Q2,
+        Q3 = request.json["Q3"],
+        Q4 = request.json["Q4"],
+        Q5 = request.json["Q5"],
+        Q6 = Q6,
+        Q7 = request.json["Q7"]
+    )
+    
     # 寫入資料庫
-    d.insertDict(data)
+    d.insertDict(dict(data))
     d.closeDB()
 
     return jsonify(str({
